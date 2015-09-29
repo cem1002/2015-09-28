493667b970cd5b778d25d7d919fd7ef897b973d6
Neil Montgomery <neilm@mie.utoronto.ca>
2015-09-29 12:34:10 -0400

    Added low correlation with clear trend example

--- corr.R
+++ corr.R
@@ -1,3 +1,4 @@
+library(rio)
 library(ggplot2)
 library(dplyr)
 library(gridExtra) # For multiple plotting
@@ -36,3 +37,35 @@ options(digits=3)
 sim1 <- cor(cor_data)
 
 # ADD LOW CORR EXAMPLE WITH CLEAR RELATIONSHIP
+# 
+# Added 2015-09-29
+# 
+# Terminology: "Low" and "High" correlation almost always means "far from 0" 
+# either positive or negative. So a correlation of -0.95 would be considered 
+# "very highly negatively correlated".
+# 
+# Also, a low correlation (close to zero) could mean that there is no linear 
+# relationship at all, or it could mean that there really is a clear 
+# relationship but with lots of random noise. Here is a contrived, simulated example:
+
+# The set.seed function ensures you get the same "random" numbers I do. Change
+# the seed to get different ones if you like.
+set.seed(2015)
+unif_noise <- runif(10000, -3, 3)
+x2 <- seq(0, 10, length.out = 10000)
+y2 <- 0.02*x2 + unif_noise
+d2 <- data_frame(x2, y2)
+
+# There is a clear increasing trend from left to right.
+ggplot(d2, aes(x2, y2)) + 
+  geom_point() + 
+  stat_smooth(method="lm", se = FALSE) +
+  theme_gray()
+
+# But the correlation is very small - because of the noise.
+cor(d2)
+
+# For those who are familiar with linear regression (we will cover this later)
+# here is the analysis to show that the linear relationship really is
+# statistically significant.
+summary(lm(y2~x2, d2))
